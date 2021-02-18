727aeda6c6af793d0c5ff3901647a24ae6c4139b
Lukas Lösche <lloesche@fedoraproject.org>
2021-02-18 02:22:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tidal-sync
+Sync TIDAL Album favorites using [tidal-dl](https://github.com/yaronzz/Tidal-Media-Downloader)
+
+Very rough script that fetches TIDAL Album favorites checks which IDs exist as local directories and feeds all missing ones to tidal-dl.
+Requires tidal-dl to be set up and authenticated.
---
+++ tidal_sync/__main__.py
@@ -0,0 +1,68 @@
+import sys
+import os
+from pathlib import Path
+import logging
+import tidal_dl
+import requests
+from aigpy.stringHelper import isNull
+from argparse import ArgumentParser
+from pprint import pprint
+
+logging.getLogger("tidal-sync").setLevel(logging.INFO)
+logging.basicConfig(
+    level=logging.WARN,
+    format="%(asctime)s - %(levelname)s - %(process)d/%(threadName)s - %(message)s",
+)
+log = logging.getLogger("tidal-sync")
+
+argv = sys.argv[1:]
+if "-v" in argv or "--verbose" in argv:
+    logging.getLogger("tidal-sync").setLevel(logging.DEBUG)
+
+
+def main() -> None:
+    arg_parser = get_arg_parser()
+    arg_parser.parse_args()
+    log.info("TIDAL-Sync starting up")
+    tidal_dl.checkLogin()
+    uri = f"https://listen.tidal.com/v1/users/{tidal_dl.TOKEN.userid}/favorites/ids"
+    header = {
+        "authorization": "Bearer {}".format(tidal_dl.TOKEN.accessToken),
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+    }
+    params = {"countryCode": tidal_dl.TOKEN.countryCode}
+    respond = requests.get(uri, headers=header, params=params)
+    favorites = respond.json()
+    album_dir = Path(tidal_dl.CONF.downloadPath + "/Album")
+    artist_dirs = [d for d in album_dir.iterdir() if d.is_dir()]
+    local_albums = []
+    downloads = []
+    for artist_dir in artist_dirs:
+        album_dirs = [d.name for d in artist_dir.iterdir() if d.is_dir()]
+        local_albums.extend(album_dirs)
+    for album in favorites.get("ALBUM", []):
+        album_str = f"[{album}]"
+        if any([s for s in local_albums if album_str in s]):
+            log.info(f"Already have {album}")
+        else:
+            log.info(f"Downloading {album}")
+            downloads.append(album)
+    for album in downloads:
+        tidal_dl.start(tidal_dl.TOKEN, tidal_dl.CONF, album)
+
+
+def get_arg_parser() -> ArgumentParser:
+    arg_parser = ArgumentParser(description="TIDAL-Sync")
+    arg_parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Verbose logging",
+        dest="verbose",
+        action="store_true",
+        default=False,
+    )
+    return arg_parser
+
+
+if __name__ == "__main__":
+    main()
